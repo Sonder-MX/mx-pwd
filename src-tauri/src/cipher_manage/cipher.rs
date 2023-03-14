ea9209af46587c66807fe8a44d425c2963abacdf
SonderMX <sonder-mx@outlook.com>
2023-03-14 22:14:48 +0800

    update other

--- src-tauri/src/cipher_manage/cipher.rs
+++ src-tauri/src/cipher_manage/cipher.rs
@@ -1,3 +1,6 @@
+// use aes_gcm::{aead::Aead, Aes256Gcm, KeyInit};
+// use base64;
+// use generic_array::GenericArray;
 use nanoid::nanoid;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
@@ -20,3 +23,24 @@ impl Cipher {
         }
     }
 }
+
+// AES加密
+// pub fn encrypt_aes(plain_text: &str, key: &str) -> String {
+//     let cipher = Aes256Gcm::new(GenericArray::from_slice(key.as_bytes()));
+//     let nonce = GenericArray::from_slice(b"unique nonce");
+//     let cipher_text = cipher
+//         .encrypt(nonce, plain_text.as_bytes().into())
+//         .expect("encryption failure!");
+//     base64::encode(cipher_text)
+// }
+
+// AES解密
+// pub fn decrypt_aes(cipher_text: &str, key: &str) -> String {
+//     let cipher = Aes256Gcm::new(GenericArray::from_slice(key.as_bytes()));
+//     let nonce = GenericArray::from_slice(b"unique nonce"); // 12 bytes; unique per message
+//     let cipher_text = base64::decode(cipher_text).unwrap();
+//     let plain_text = cipher
+//         .decrypt(nonce, cipher_text.as_slice().into())
+//         .expect("decryption failure!");
+//     String::from_utf8(plain_text).unwrap()
+// }
