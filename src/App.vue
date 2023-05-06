f8fd2bd028012f64d5ec4d32c934a471d4b470ef
SonderMX <sonder-mx@outlook.com>
2023-05-06 21:49:30 +0800

    install tauri-apps/api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <div>
+    <button @click="getCipherList">get cipher list</button>
+  </div>
+  <div>
+    <button @click="getCipherDetail">get cipher detail</button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { invoke } from "@tauri-apps/api"
 
+const getCipherList = () => {
+  invoke("get_cipher_list")
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+const getCipherDetail = () => {
+  invoke("get_cipher_detail", { dnid: "1" })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 </script>
