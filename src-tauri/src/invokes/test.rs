d07290e4694992cc49af0da604f8cf837dea85f2
SonderMX <sonder-mx@outlook.com>
2023-05-07 20:24:11 +0800

    test todo....

---
+++ src-tauri/src/invokes/test.rs
@@ -0,0 +1,16 @@
+// File: test.rs
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test_cipher_list_api() {
+        todo!();
+    }
+
+    #[test]
+    fn test_cipher_add_api() {
+        todo!();
+    }
+}
